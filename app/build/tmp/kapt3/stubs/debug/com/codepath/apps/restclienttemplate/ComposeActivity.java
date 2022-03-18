d1285ec0e752f2bfb5a233987cbb1d0db9b73599
crowleyg <crowleyg@oregonstate.edu>
2022-03-18 09:42:14 -0700

    week 2 of development

---
+++ app/build/tmp/kapt3/stubs/debug/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -0,0 +1,63 @@
+package com.codepath.apps.restclienttemplate;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u00002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u0000 \u00192\u00020\u0001:\u0001\u0019B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\u0015\u001a\u00020\u00162\b\u0010\u0017\u001a\u0004\u0018\u00010\u0018H\u0014R\u001a\u0010\u0003\u001a\u00020\u0004X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\bR\u001a\u0010\t\u001a\u00020\nX\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u000b\u0010\f\"\u0004\b\r\u0010\u000eR\u001a\u0010\u000f\u001a\u00020\u0010X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0011\u0010\u0012\"\u0004\b\u0013\u0010\u0014\u00a8\u0006\u001a"}, d2 = {"Lcom/codepath/apps/restclienttemplate/ComposeActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "btnTweet", "Landroid/widget/Button;", "getBtnTweet", "()Landroid/widget/Button;", "setBtnTweet", "(Landroid/widget/Button;)V", "client", "Lcom/codepath/apps/restclienttemplate/TwitterClient;", "getClient", "()Lcom/codepath/apps/restclienttemplate/TwitterClient;", "setClient", "(Lcom/codepath/apps/restclienttemplate/TwitterClient;)V", "etCompose", "Landroid/widget/EditText;", "getEtCompose", "()Landroid/widget/EditText;", "setEtCompose", "(Landroid/widget/EditText;)V", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "Companion", "app_debug"})
+public final class ComposeActivity extends androidx.appcompat.app.AppCompatActivity {
+    public android.widget.EditText etCompose;
+    public android.widget.Button btnTweet;
+    public com.codepath.apps.restclienttemplate.TwitterClient client;
+    @org.jetbrains.annotations.NotNull()
+    public static final com.codepath.apps.restclienttemplate.ComposeActivity.Companion Companion = null;
+    @org.jetbrains.annotations.NotNull()
+    private static final java.lang.String TAG = "ComposeActivity";
+    
+    public ComposeActivity() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final android.widget.EditText getEtCompose() {
+        return null;
+    }
+    
+    public final void setEtCompose(@org.jetbrains.annotations.NotNull()
+    android.widget.EditText p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final android.widget.Button getBtnTweet() {
+        return null;
+    }
+    
+    public final void setBtnTweet(@org.jetbrains.annotations.NotNull()
+    android.widget.Button p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.codepath.apps.restclienttemplate.TwitterClient getClient() {
+        return null;
+    }
+    
+    public final void setClient(@org.jetbrains.annotations.NotNull()
+    com.codepath.apps.restclienttemplate.TwitterClient p0) {
+    }
+    
+    @java.lang.Override()
+    protected void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    @kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u0003\b\u0086\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002R\u0014\u0010\u0003\u001a\u00020\u0004X\u0086D\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0005\u0010\u0006\u00a8\u0006\u0007"}, d2 = {"Lcom/codepath/apps/restclienttemplate/ComposeActivity$Companion;", "", "()V", "TAG", "", "getTAG", "()Ljava/lang/String;", "app_debug"})
+    public static final class Companion {
+        
+        private Companion() {
+            super();
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        public final java.lang.String getTAG() {
+            return null;
+        }
+    }
+}
